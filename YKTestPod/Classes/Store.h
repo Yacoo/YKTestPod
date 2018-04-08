4386d6ee703ef7e1d01f8a5dc71c59a19d65a892
yake <yake_0223@126.com>
2018-04-08 15:16:13 +0800

    change class

---
+++ YKTestPod/Classes/Store.h
@@ -0,0 +1,15 @@
+//
+//  Store.h
+//  test
+//
+//  Created by Yake on 2017/4/23.
+//  Copyright © 2017年 XF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Store : NSObject
+
+@property(nonatomic, strong) NSArray * merchandise;
+
+@end
---
+++ YKTestPod/Classes/Store.m
@@ -0,0 +1,13 @@
+//
+//  Store.m
+//  test
+//
+//  Created by Yake on 2017/4/23.
+//  Copyright © 2017年 XF. All rights reserved.
+//
+
+#import "Store.h"
+
+@implementation Store
+
+@end
